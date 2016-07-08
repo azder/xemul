7f8c0aff2d0e749664964fece740bdd0047f891d
azder <azhder@gmail.com>
2016-07-08 12:22:27 +0200

    added CMake'd stub for the project

---
+++ src/custom.h
@@ -0,0 +1,9 @@
+//
+// Created by azder on 01/07/2016.
+//
+
+#ifndef XEMUL_CUSTOM_H
+#define XEMUL_CUSTOM_H
+
+
+#endif //XEMUL_CUSTOM_H
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "custom.h"
+
+
+int main() {
+
+
+    return 0;
+
+}
