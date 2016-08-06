9e2963b90db6b54b494aaf6b87c3c3bd91645f6e
mario <velickovskimario@outlook.com>
2016-08-06 15:51:38 +0200

    Add MODIFY_BIT wrapper

--- src/bitwise.h
+++ src/bitwise.h
@@ -2,3 +2,4 @@
 #define CHECK_BIT(var,pos) ((var >> pos) & 1)
 #define SET_BIT(var,pos) (var |= 1 << pos);
 #define UNSET_BIT(var,pos) if(CHECK_BIT(var, pos)) (var ^= 1 << pos);
+#define MODIFY_BIT(var,pos,val) if(val) SET_BIT(var, pos) else UNSET_BIT(var,pos);
